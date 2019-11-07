fe0dfd348d344efb6469b123cd1c9892fbf9d9e2
saritha <saritha@gmai.com>
2019-11-06 22:40:46 -0600

    developer added new file to branch

---
+++ mavengithub/src/main/java/com/demo/mavengithub/TestClassAddedToBranch.java
@@ -0,0 +1,9 @@
+package com.demo.mavengithub;
+
+public class TestClassAddedToBranch {
+	  public static void main( String[] args )
+	    {
+	        System.out.println( "TestClass added to branch" );
+	    }
+
+}
