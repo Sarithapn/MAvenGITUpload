fb3b559c0ea522e0acd6c27e98844fa043c2981f
saritha <saritha@gmai.com>
2019-11-06 22:06:52 -0600

    first commit

---
+++ mavengithub/src/main/java/com/demo/mavengithub/App.java
@@ -0,0 +1,16 @@
+package com.demo.mavengithub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        String[] names={"apple","ball","cat"};
+        for(int i=0;i<names.length;i++)
+        System.out.println(names[i]);
+    }
+}
